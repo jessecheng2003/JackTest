4f2c7ba74f95d088575a8e79db4516f2e2a9d711
JACK <jack_cheng_2000@yahoo.com>
2018-08-30 17:43:08 -0400

    Adding the playbook and the roles for Apache and PHP 7

---
+++ roles/php/files/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <title> eclome to DevOps on AWS</title>title>
+</head>
+<body>
+	<h1><?php echo "Weclome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
+</body>
+</html>
+
